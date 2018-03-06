5246c07a156db2f629a978a9f67a7943718a8026
TheGhostSpirit <quentin.caritey@outlook.fr>
2018-03-06 17:46:12 +0100

    Added router

--- web/src/app.js
+++ web/src/app.js
@@ -1,5 +1,14 @@
 export class App {
+  
   constructor() {
-    this.message = 'Hello World!';
+  }
+
+  configureRouter(config, router){
+    config.map([
+      { route: ['game', ''], moduleId: 'views/game',   title: 'Game'},
+      { route: 'leaderboard',  moduleId: 'views/leaderboard', name:'Leaderboard' }
+    ]);
+
+    this.router = router;
   }
 }
