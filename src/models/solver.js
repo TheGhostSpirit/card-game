22104398a765fd68facc2e2aba72fdd724e4952a
Vincent CARITEY <vincent.caritey@kisss.fr>
2018-10-20 03:12:23 +0200

    Try to resolve a resolvable game

--- src/models/solver.js
+++ src/models/solver.js
@@ -29,8 +29,11 @@ export class Solver {
   }
 
   initialize() {
-    this.solitaire.newGame();
-    this.solitaire.fullyTurnStub();
+    this.solitaire.initialize();
+    let game = JSON.parse('{"kingSlots":[[],[],[],[]],"slots":[[{"suit":"diams","name":"3","returned":false}],[{"suit":"clubs","name":"9","returned":true},{"suit":"diams","name":"10","returned":false}],[{"suit":"hearts","name":"9","returned":true},{"suit":"spades","name":"2","returned":true},{"suit":"hearts","name":"6","returned":false}],[{"suit":"hearts","name":"4","returned":true},{"suit":"clubs","name":"10","returned":true},{"suit":"clubs","name":"5","returned":true},{"suit":"spades","name":"K","returned":false}],[{"suit":"clubs","name":"4","returned":true},{"suit":"diams","name":"2","returned":true},{"suit":"diams","name":"K","returned":true},{"suit":"hearts","name":"Q","returned":true},{"suit":"spades","name":"10","returned":false}],[{"suit":"clubs","name":"A","returned":true},{"suit":"hearts","name":"5","returned":true},{"suit":"clubs","name":"6","returned":true},{"suit":"clubs","name":"3","returned":true},{"suit":"hearts","name":"8","returned":true},{"suit":"diams","name":"8","returned":false}],[{"suit":"spades","name":"3","returned":true},{"suit":"clubs","name":"Q","returned":true},{"suit":"hearts","name":"A","returned":true},{"suit":"spades","name":"J","returned":true},{"suit":"diams","name":"7","returned":true},{"suit":"spades","name":"Q","returned":true},{"suit":"hearts","name":"2","returned":false}]],"stub":{"cards":[],"returnedCards":[{"suit":"diams","name":"4","returned":false},{"suit":"hearts","name":"3","returned":false},{"suit":"clubs","name":"7","returned":false},{"suit":"spades","name":"7","returned":false},{"suit":"clubs","name":"2","returned":false},{"suit":"hearts","name":"J","returned":false},{"suit":"spades","name":"8","returned":false},{"suit":"spades","name":"5","returned":false},{"suit":"spades","name":"A","returned":false},{"suit":"hearts","name":"K","returned":false},{"suit":"clubs","name":"J","returned":false},{"suit":"diams","name":"J","returned":false},{"suit":"spades","name":"9","returned":false},{"suit":"clubs","name":"8","returned":false},{"suit":"hearts","name":"10","returned":false},{"suit":"clubs","name":"K","returned":false},{"suit":"diams","name":"Q","returned":false},{"suit":"diams","name":"9","returned":false},{"suit":"diams","name":"5","returned":false},{"suit":"diams","name":"6","returned":false},{"suit":"spades","name":"4","returned":false},{"suit":"diams","name":"A","returned":false},{"suit":"spades","name":"6","returned":false},{"suit":"hearts","name":"7","returned":false}]}}');
+    this.solitaire.restore(game);
+    //this.solitaire.newGame();
+    //this.solitaire.fullyTurnStub();
     this.shadowSolitaire.initialize();
     this.shadowSolitaire.restore(this.solitaire.dump());
   }
@@ -163,25 +166,40 @@ export class Solver {
         description: `${c.symbol}:stub->slot${i + 1}`
       }));
     }
-    for (let i = 0; i < kingSlotNumber; i++) {
-      for (let j = 0; j < slotNumber; j++) {
-        let emptyDestinationSlot = this.solitaire.slots[j].cards.length === 0;
-        if (emptyDestinationSlot) continue;
-        let c = this.solitaire.kingSlots[i].cards[this.solitaire.kingSlots[i].cards.length - 1];
-        if (c && this.solitaire.slots[j].canMoveTo([c])) {
-          moves.push({
-            source: this.solitaire.kingSlots[i].cards,
-            destination: this.solitaire.slots[j].cards,
-            selection: [c],
-            zone: ZONES.kingSlots,
-            description: `${c.symbol}:kingslot->slot${j + 1}`
-          });
+    if (moves.length === 0) {
+      for (let i = 0; i < kingSlotNumber; i++) {
+        for (let j = 0; j < slotNumber; j++) {
+          let emptyDestinationSlot = this.solitaire.slots[j].cards.length === 0;
+          if (emptyDestinationSlot) continue;
+          let c = this.solitaire.kingSlots[i].cards[this.solitaire.kingSlots[i].cards.length - 1];
+          if (c && this.solitaire.slots[j].canMoveTo([c])) {
+            moves.push({
+              source: this.solitaire.kingSlots[i].cards,
+              destination: this.solitaire.slots[j].cards,
+              selection: [c],
+              zone: ZONES.kingSlots,
+              description: `${c.symbol}:kingslot->slot${j + 1}`
+            });
+          }
         }
       }
     }
     let map = new Map();
     moves.forEach(m => map.set(m.description, m));
     return Array.from(map.values());
+  }
+
+  doMove(move, message, possibleMoves) {
+    this.pushState(message, possibleMoves);
+    //let toState = this.solitaire.dump();
+    this.solitaire.doMove(move.source, move.destination, move.selection, move.zone);
+    //let fromState = this.solitaire.dump();
+    return JSON.stringify(this.solitaire.dump());//move.description; // JSON.stringify({from: toState, to: fromState});
+  }
+
+  undoLastMove(message, possibleMoves) {
+    this.pushState(message, possibleMoves, true);
+    this.solitaire.undoMove();
   }
 
   resolveStep(n) {
@@ -193,12 +211,9 @@ export class Solver {
     if (movesCount > 0) {
       for (let i = 0; i < movesCount; i++) {
         let move = possibleMoves[i];
-        this.pushState(`E${n}-m${i + 1}/${movesCount}=${move.description}`, possibleMoves);
-        this.solitaire.doMove(move.source, move.destination, move.selection, move.zone);
-        let state = JSON.stringify(this.solitaire.dump());
+        let state = this.doMove(move, `+E${n}-m${i + 1}/${movesCount}=${move.description}`, possibleMoves);
         if (this.set.has(state)) {
-          this.pushState(`E${n}-m${i + 1}/${movesCount}=${move.description}:CYCLE:UNDONE`, possibleMoves, true);
-          this.solitaire.undoMove();
+          this.undoLastMove(`-E${n}-m${i + 1}/${movesCount}=${move.description}:CYCLE`, possibleMoves);
           continue;
         } else {
           this.set.add(state);
@@ -209,11 +224,9 @@ export class Solver {
         let res = this.resolveStep(n + 1);
         if (res) return true;
       }
-      this.pushState(`E${n}:ENDLOOP:UNDONE`, possibleMoves);
-      this.solitaire.undoMove();
+      this.undoLastMove(`-E${n}:ENDLOOP`, possibleMoves);
     } else {
-      this.pushState(`E${n}:NOSOLUTION:UNDONE`, possibleMoves);
-      this.solitaire.undoMove();
+      this.undoLastMove(`-E${n}:NOSOLUTION`, possibleMoves);
     }
   }
 }
