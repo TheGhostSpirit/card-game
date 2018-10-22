9e6a7acc4c6a717d6705c115290a8b782848866f
Vincent CARITEY <vincent.caritey@kisss.fr>
2018-10-22 15:51:47 +0200

    Fixed autox100 launch

--- src/views/auto100.js
+++ src/views/auto100.js
@@ -7,30 +7,26 @@ export class Auto100 {
   constructor(solver, service) {
     this.service = service;
     this.solver = solver;
-    this.wins = 0;
     this.awaitedWins = 0;
-    this.percentage = 0;
-    this.amount = 100;
     this.games = [];
   }
 
   activate() {
     this.service.getGames().then(result => {
       this.games = result;
-      this.solver.load(this.games[0].game);
-      this.solitaire = this.solver.shadowSolitaire;
-      this.games.forEach(g => {
-        if (g.result) this.awaitedWins++;
-      });
+      this.amount = result.length;
+      this.awaitedWins = this.games.filter(g => g.result).length;
     });
   }
 
   launch() {
+    this.wins = 0;
+    this.percentage = 0;
     let beginTime = Date.now();
     for (let i = 0; i < this.amount; i++) {
+      this.solver.load(this.games[i].game);
       let outcome = this.solver.resolve();
       if (outcome) this.wins++;
-      this.solver.load(this.games[i].game);
     }
     let finishTime = Date.now();
     this.time = ((finishTime - beginTime) / 1000);
